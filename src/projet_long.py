53509ad7e81d0e1bddeb12c4d0a12ea753cb408b
galtos <guillaume.ollitrault@gmail.com.com>
2019-12-23 19:43:04 +0100

    add functions for rsa calculation and surface, interface interaction, and QIPI values

--- src/projet_long.py
+++ src/projet_long.py
@@ -6,9 +6,12 @@
 
 from Bio.PDB import * # set ref
 import numpy as np
-import freesasa
 
-
+QIPI = {
+'H':1.147, 'R':1.346, 'K':0.784, 'A':0.841, 'V':0.994, 'I':1.084, 'L':1.144, \
+'M':1.451, 'P':1.109, 'F':1.334, 'W':1.284, 'Y':1.368, 'G':0.823, 'C':1.172, \
+'S':0.873, 'T':0.966, 'N':0.958, 'Q':0.909, 'D':0.830, 'E':0.805}
+									
 # get pdb file names
 
 def get_structure(name, path, str_structure_id):
@@ -21,7 +24,7 @@ def get_sequence(structure):
     for pp in ppb.build_peptides(structure):
         polypeptide = pp.get_sequence()
     return(list(polypeptide))
-
+"""
 def get_surface_residue(structure, path_structure, rsa_threshold, acc_array = "Sander"):
     list_surface_residue = []
     dssp = DSSP(structure, path_structure, acc_array = acc_array)
@@ -43,6 +46,7 @@ def get_surface_residue(structure, path_structure, rsa_threshold, acc_array = "S
         list_surface_residue.append(vec_surface_residue)
         
     return list_surface_residue
+"""
 
 
 def residue_distance(residue_1, residue_2):
@@ -51,7 +55,7 @@ def residue_distance(residue_1, residue_2):
     except KeyError:
         print("no CA atoms")
     return distance
-
+"""
 def get_interface_residue(structure_1, structure_2, distance_threshold = 8):
     residues_1 = [r for r in structure_1.get_residues()]
     residues_2 = [r for r in structure_2.get_residues()]
@@ -67,6 +71,7 @@ def get_interface_residue(structure_1, structure_2, distance_threshold = 8):
                 vector_interface_1[i] = 1
                 vector_interface_2[j] = 1
     return(list_interface_residue, vector_interface_1, vector_interface_2)
+"""
 
 def get_neighbor_residues(structure, residue_middle, k_threshold=9):
     residues = [r for r in structure.get_residues()]
@@ -75,6 +80,58 @@ def get_neighbor_residues(structure, residue_middle, k_threshold=9):
         distance = residue_middle["CA"] - residues[i]["CA"]#residue_distance(residue_middle, residues[i]) #ERROR with function
         residue_distance.append((i,distance))
     return(sorted(residue_distance, key=itemgetter(1))[0:k_threshold])
+    
+def get_rsa_relative(path_file_rsa):
+    list_relative_rsa = []
+    for line in open(path_file_rsa):
+        list = line.split()
+        id = list[0]
+        if id == 'RES':
+            relative_rsa = list[5]
+            list_relative_rsa.append(float(relative_rsa))
+    return(list_relative_rsa)
+
+def get_rsa_relative_bind(path_file_rsa):
+    list_relative_rsa = [[]]
+    struct_flag = 2
+    for line in open(path_file_rsa):
+        list = line.split()
+        id = list[0]
+        
+        if id == 'RES':
+            res_number = float(list[3])
+            if struct_flag == 2:
+                struct_flag = 0
+            elif struct_flag == 0 and res_number != (res_number_previous+1):
+                struct_flag = 1
+                list_relative_rsa.append([])
+            relative_rsa = list[5]
+            list_relative_rsa[struct_flag].append(float(relative_rsa))
+            res_number_previous = float(list[3])
+    return((list_relative_rsa[0],list_relative_rsa[1]))
+    
+def get_surface_residue(list_relative_rsa, threshold_rsa = 0.1):
+    list_surface_residue = []
+    for i in range(len(list_relative_rsa)):
+        if list_relative_rsa[i] >= threshold_rsa:
+            list_surface_residue.append(i+1)
+    return(list_surface_residue)
+    
+def get_interface_residue(list_relative_rsa, list_relative_rsa_bind):
+    list_interface_residue = []
+    for i in range(len(list_relative_rsa)):
+        if list_relative_rsa[i] != list_relative_rsa_bind[i]:
+            list_interface_residue.append(i+1)
+    return(list_interface_residue)
+            
+def count_intersect_surface_interface_residue(list_surface_residue, list_interface_residue):
+    k = 0
+    for i in range(len(list_surface_residue)):
+        for j in range(len(list_interface_residue)):
+            if list_surface_residue[i] == list_interface_residue[j]:
+                k = k+1
+    print(k)
+#def get_pssm_value(path_pssm):
     
     
 #### MAIN ####
@@ -85,23 +142,27 @@ if __name__ == "__main__":
     with open(path_list_bound_pdb_file) as file:
         list_bound_pdb_file = file.readlines()
 
-    print(list_unbound_pdb_file[0], list_bound_pdb_file[0])
+    print(list_bound_pdb_file[0])
 
     structure_1 = []
     structure_2 = []
     structure_12 = []
-    structure_1.append(get_structure('test_bound_1', path_bound + "/templates/" + list_bound_pdb_file[0][0:-1], '_1.pdb')
+    structure_1.append(get_structure('test_bound_1', path_bound + "/templates/" + list_bound_pdb_file[0][0:-1], '_1.pdb'))
     structure_2.append(get_structure('test_bound_2', path_bound + "/templates/" + list_bound_pdb_file[0][0:-1], '_2.pdb'))
     structure_12.append(get_structure('test_bound_12', path_bound + "/templates_bind/" + list_bound_pdb_file[0][0:-1],'.pdb'))
+    ####
+    # distance : test, vec1, vec2 = get_interface_residue(structure_1[0], structure_2[0])
+    #list_surface_residue = get_surface_residue(structure_1[0][0], path_bound + "/templates/" + list_bound_pdb_file[0][0:-1] + '_1.pdb', 0.1)
+    path_file_rsa = path_bound + "/templates/" + list_bound_pdb_file[0][0:-1] + '_1.rsa'
+    path_file_rsa_bind = path_bound + "/templates_bind/" + list_bound_pdb_file[0][0:-1] + '.rsa'
     
-    test, vec1, vec2 = get_interface_residue(structure_1[0], structure_2[0])
+    list_relative_rsa = get_rsa_relative(path_file_rsa)
+    list_relative_rsa_bind = get_rsa_relative_bind(path_file_rsa_bind)
+    print(list_relative_rsa)
+    print(list_relative_rsa_bind[0])
+    list_surface_residue = get_surface_residue(list_relative_rsa)
+    list_interface_residue = get_interface_residue(list_relative_rsa, list_relative_rsa_bind[0])
     
-    list_surface_residue = get_surface_residue(structure_1[0][0], path_bound + "/templates/" + list_bound_pdb_file[0][0:-1] + '_1.pdb', 0.1)
-
-    list_surface_residue_12 = get_surface_residue(structure_12[0][0], path_bound + "/templates/" + list_bound_pdb_file[0][0:-1] + '_1.pdb', 0.1)
-
-    list_surface_residue_12[1]
-
     res = [value for value in list_surface_residue if value not in list_surface_residue_12[1]]
 
     for i in range(len(list_surface_residue)):
